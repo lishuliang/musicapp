83585567c556f1ce4be7d7f729cd14e767a8876b
Li Shuliang <532784460@qq.com>
2022-06-30 14:24:46 +0800

    icon list component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,14 @@
 <script setup>
 import topNav from "./topNav.vue";
 import swipeCom from './swipeCom.vue'
+import iconList from './iconList.vue'
 </script>
 
 <template>
   <div class="home">
     <top-nav></top-nav>
     <swipe-com></swipe-com>
+    <icon-list></icon-list>
   </div>
 </template>
 
---
+++ src/components/iconList.vue
@@ -0,0 +1,51 @@
+<script setup>
+</script>
+
+<template>
+  <div class="iconList">
+    <div class="iconItem">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-xiangsituijian"></use>
+      </svg>
+      <span>每日推荐</span>
+    </div>
+    <div class="iconItem">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-FM_weixuanze"></use>
+      </svg>
+      <span>私人FM</span>
+    </div>
+    <div class="iconItem">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-gedan"></use>
+      </svg>
+      <span>歌单</span>
+    </div>
+    <div class="iconItem">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-paihangbang"></use>
+      </svg>
+      <span>排行榜</span>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.iconList {
+  display: flex;
+  justify-content: space-between;
+  padding: 0.4rem;
+  .iconItem {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .icon {
+      width: 1rem;
+      height: 1rem;
+    }
+    span{
+        font-size: 0.26rem;
+    }
+  }
+}
+</style>
